093b90fd253c314aab22b54d7afb0e886a35c070
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-25 19:01:33 -0300

    add "news" section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.scss';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Categories from './components/Categories';
+import News from './components/News';
 
 const App = () => (
   <>
     <Header />
     <Banner />
     <Categories />
+    <News />
   </>
 );
 
