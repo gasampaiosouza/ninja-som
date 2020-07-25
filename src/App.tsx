123d7e66554d2da64a3bcc8d569648d3d7198194
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-25 18:16:06 -0300

    :tada: add categories section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.scss';
+
 import Header from './components/Header';
+import Banner from './components/Banner';
+import Categories from './components/Categories';
 
 const App = () => (
   <>
     <Header />
+    <Banner />
+    <Categories />
   </>
 );
 
