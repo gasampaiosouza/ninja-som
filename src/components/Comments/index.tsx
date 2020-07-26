5b4aedfb7eba8dfd8cf9c4bf8c0671a879a44759
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 17:24:34 -0300

    finish comment section

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './style.scss';
+
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+
+import userImage from '../../assets/user-image.png';
+
+const Comments: React.FC = () => {
+  const users = [
+    {
+      image: userImage,
+      name: 'Felipe Salmim',
+      role: 'Designer',
+      comment: `Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+      and a search for ‘lorem ipsum’`,
+    },
+    {
+      image: userImage,
+      name: 'Felipe Salmim',
+      role: 'Designer',
+      comment: `Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+      and a search for ‘lorem ipsum’`,
+    },
+  ];
+
+  return (
+    <section id="comments">
+      <div className="wrapper">
+        <Slider infinite={true}>
+          {users.map((user) => (
+            <div className="comments-box">
+              <div className="user">
+                <img src={user.image} alt="user-image" className="user-image" />
+                <div className="user-content">
+                  <span className="user-name bold">{user.name}</span>
+                  <small className="user-role">{user.role}</small>
+                </div>
+              </div>
+              <div className="comment">
+                <p>{user.comment}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
+};
+
+export default Comments;
