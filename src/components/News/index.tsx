852b85a53f7f94cc15f0803359df2eb961576ed1
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 17:25:37 -0300

    finish news section

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 import './style.scss';
 
 const News: React.FC = () => {
+  const news = [
+    { title: 'ÁUDIO', subTitle: 'PROFISSIONAL', independentClass: 'audio-box' },
+    {
+      title: 'INSTRUMENTOS',
+      subTitle: 'MÚSICAIS',
+      independentClass: 'instruments-box',
+    },
+  ];
+
   return (
-    <div>
-      <h1>news bro</h1>
+    <div id="news">
+      {news.map(({ title, subTitle, independentClass }) => (
+        <div className={`news-box ${independentClass}`}>
+          <small className="label light">NOVIDADES</small>
+          <h1 className="title bold">{title}</h1>
+          <h1 className="sub-title regular">{subTitle}</h1>
+
+          <button className="btn outlined">CONFIRA</button>
+        </div>
+      ))}
     </div>
   );
 };
