af7a50cbe8646966b13f107b60746397db24628f
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 20:57:49 -0300

    add fade animation

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import './style.scss';
 
+import Fade from 'react-reveal/Fade';
+
 const About: React.FC = () => {
   return (
-    <div id="about">
-      <h1 className="section-title">SOBRE NÓS</h1>
-      <p className="section-paragraph">
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using ‘Content here, content here’, making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for ‘lorem ipsum’ will uncover many web sites still in their
-        infancy. Lorem Ipsum as their default model text, and a search for
-        ‘lorem ipsum’ will uncover many web sites still in their infancy. Lorem
-        Ipsum as their default model text, and a search for ‘lorem ipsum’ will
-        uncover many web sites still in their infancy.
-      </p>
-    </div>
+    <Fade top cascade>
+      <div id="about">
+        <h1 className="section-title">SOBRE NÓS</h1>
+        <p className="section-paragraph">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using ‘Content here, content here’, making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for ‘lorem ipsum’ will uncover many web sites still in their
+          infancy. Lorem Ipsum as their default model text, and a search for
+          ‘lorem ipsum’ will uncover many web sites still in their infancy.
+          Lorem Ipsum as their default model text, and a search for ‘lorem
+          ipsum’ will uncover many web sites still in their infancy.
+        </p>
+      </div>
+    </Fade>
   );
 };
 
