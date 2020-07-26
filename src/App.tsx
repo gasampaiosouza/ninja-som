8ea0d50593256c35200bcd6d82f4b89fa9ff1f68
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 17:22:48 -0300

    add all the remaining sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Categories from './components/Categories';
 import News from './components/News';
+import Products from './components/Products';
+import Companies from './components/Companies';
+import About from './components/About';
+import Comments from './components/Comments';
+import Footer from './components/Footer';
 
 const App = () => (
   <>
@@ -12,6 +17,11 @@ const App = () => (
     <Banner />
     <Categories />
     <News />
+    <Products />
+    <Companies />
+    <About />
+    <Comments />
+    <Footer />
   </>
 );
 
