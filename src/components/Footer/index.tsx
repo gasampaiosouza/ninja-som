947ea6c0953ff516db7b031b2a2a4c574c90f9a4
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 17:25:16 -0300

    :tada: footer initial commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer id="footer">
+      <h1>footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
