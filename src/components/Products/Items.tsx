2c9f461e85e9e364bb45f7ad8c277ca05eeb72ea
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-26 23:00:01 -0300

    :scissors: refactored all the code

--- src/components/Products/Items.tsx
+++ src/components/Products/Items.tsx
@@ -1,37 +1,30 @@
 import React, { useState, memo } from 'react';
-import dataType from '../../interfaces';
+import dataType, { modalType } from '../../interfaces';
 
-import Fade from 'react-reveal/Fade';
 import './style.scss';
 import './modal.scss';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import fixPrice from './fixPrice';
 
+import ModalContent from './ModalContent';
+import Product from './Product';
+
 interface itemsType {
   data: dataType[];
-}
-
-interface modalType extends dataType {
-  open: boolean;
 }
 
 const Items = ({ data }: itemsType) => {
   const [info, setInfo] = useState<modalType>({ open: false });
 
-  const openInfo = ({
-    open,
-    photo,
-    productName,
-    price,
-    descriptionShort,
-  }: modalType) =>
+  const openInfo = (data: modalType) =>
     setInfo({
-      open,
-      photo,
-      productName,
-      price,
-      descriptionShort,
+      /* i think it's better without destructuring, for reading purposes */
+      open: data.open,
+      photo: data.photo,
+      productName: data.productName,
+      price: data.price,
+      descriptionShort: data.descriptionShort,
     });
 
   const closeInfo = () => setInfo({ open: false });
@@ -39,59 +32,34 @@ const Items = ({ data }: itemsType) => {
   if (!data) return <h1>loading...</h1>;
 
   return (
-    <Fade right cascade>
-      <div id="products">
-        {data.map(({ photo, productName, price, descriptionShort }, index) => {
-          const priceWithComma = fixPrice(price!.toString());
+    <div id="products">
+      {data.map(({ photo, productName, price, descriptionShort }, index) => {
+        const priceWithComma = fixPrice(price!.toString());
 
-          return (
-            <div className="products-box" key={index}>
-              <img className="image" src={photo} alt={`Foto ${productName}`} />
-              <div
-                className="search-overlay"
-                onClick={() =>
-                  openInfo({
-                    open: true,
-                    photo,
-                    productName,
-                    price: priceWithComma,
-                    descriptionShort,
-                  })
-                }
-              >
-                <i className="fas fa-search search-overlay-icon"></i>
-                <small className="search-overlay-description">Quick view</small>
-                <div className="overlay"></div>
-              </div>
-              <h2 className="title">{productName}</h2>
-              <p className="description light">{descriptionShort}</p>
-              <h1 className="price">{`R$ ${priceWithComma}`}</h1>
-            </div>
-          );
-        })}
+        return (
+          <Product
+            key={index}
+            openInfo={openInfo}
+            data={{
+              open: true,
+              photo,
+              productName,
+              price: priceWithComma,
+              descriptionShort,
+            }}
+          />
+        );
+      })}
 
-        <Modal
-          onClose={closeInfo}
-          open={info.open}
-          center={true}
-          animationDuration={250}
-        >
-          <div className="modal-box">
-            <img src={info.photo} alt={info.productName} />
-            <div className="content">
-              <h1 className="title">{info.productName}</h1>
-              <span className="price bold">{info.price}</span>
-              <p className="description">{info.descriptionShort}</p>
-              <a href="#" className="link">
-                Veja mais detalhes do produto
-              </a>
-
-              <button className="btn solid">Adicionar ao carrinho</button>
-            </div>
-          </div>
-        </Modal>
-      </div>
-    </Fade>
+      <Modal
+        onClose={closeInfo}
+        open={info.open}
+        center={true}
+        animationDuration={250}
+      >
+        <ModalContent data={info} />
+      </Modal>
+    </div>
   );
 };
 
